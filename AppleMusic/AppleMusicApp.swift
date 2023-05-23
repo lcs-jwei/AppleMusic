d5a0e3c5bf3978afdb4b53b800028186ac85caf2
Justin <jwei@lcs.on.ca>
2023-05-23 11:36:19 -0400

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Justin Zack Wei on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
